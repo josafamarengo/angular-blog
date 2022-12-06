53c3a49db26602d3c6b8c295a0508fec2c8bb183
josafamarengo <josafabmarengo@gmail.com>
2022-12-06 17:30:07 -0400

    feat: add big-card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
 })
 export class BigCardComponent {
 
+  @Input()
+  coverImage:string = '';
+  @Input()
+  title:string = 'Angular Blog';
+  @Input()
+  description:string = 'A blog about Angular';
+  @Input()
+  link:string = 'https://josafa.com.br/blog';
+  @Input()
+  date:string = '2020-01-01';
 }
